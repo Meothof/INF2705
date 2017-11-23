682d35ebe35a34d576f78c6761e2385c671e4614
Théo Moffelein <thmof@l4712-04.info.polymtl.ca>
2017-11-23 16:11:44 -0500

    fin de la seance 1 tp5

---
+++ tp5-tessellation/src/nuanceurFragments.glsl
@@ -0,0 +1,87 @@
+//#version 410
+
+// Définition des paramètres des sources de lumière
+layout (std140) uniform LightSourceParameters
+{
+   vec4 ambient;
+   vec4 diffuse;
+   vec4 specular;
+   vec4 position[2];
+} LightSource;
+
+// Définition des paramètres des matériaux
+layout (std140) uniform MaterialParameters
+{
+   vec4 emission;
+   vec4 ambient;
+   vec4 diffuse;
+   vec4 specular;
+   float shininess;
+} FrontMaterial;
+
+// Définition des paramètres globaux du modèle de lumière
+layout (std140) uniform LightModelParameters
+{
+   vec4 ambient;       // couleur ambiante
+   bool localViewer;   // observateur local ou à l'infini?
+   bool twoSide;       // éclairage sur les deux côtés ou un seul?
+} LightModel;
+
+uniform sampler2D textureCoul;
+
+// bool gl_FrontFacing  // variable standard d'OpenGL: c'est la face avant qui est actuellement tracée?
+
+in Attribs {
+   vec2 texCoord;
+   vec3 normale, lumiDir[2], obsVec;
+} AttribsIn;
+
+out vec4 FragColor;
+
+bool utiliseBlinn = true;  // on utilise toujours Blinn
+
+void main( void )
+{
+   vec4 coul = FrontMaterial.emission + FrontMaterial.ambient * LightModel.ambient;
+
+   // vecteur normal
+   vec3 N = normalize( gl_FrontFacing ? AttribsIn.normale : -AttribsIn.normale );
+
+   // calculer la contribution de chaque source lumineuse
+   for ( int i = 0 ; i < 2 ; ++i )
+   {
+      // ajouter la contribution de la composante ambiante
+      coul += FrontMaterial.ambient * LightSource.ambient;
+
+      vec3 L = normalize(AttribsIn.lumiDir[i]);
+
+      float NdotL = dot(N, L);
+
+      // calcul de l'éclairage seulement si le produit scalaire est positif
+      if ( NdotL > 0.0 )
+      {
+         // ajouter la contribution de la composante diffuse
+#if ( INDICEDIFFUSE == 0 )
+         // la composante diffuse (kd) du matériel est utilisé
+         coul += FrontMaterial.diffuse * LightSource.diffuse * NdotL;
+#else
+         // la composante diffuse (kd) provient de la texture 'textureCoul'
+         //coul += ...
+#endif
+
+         // ajouter la contribution de la composante spéculaire
+         // produit scalaire pour la réflexion spéculaire (selon Blinn)
+         vec3 O = normalize(AttribsIn.obsVec);
+         float NdotHV = max( 0.0, dot( normalize( L + O ), N ) );
+         coul += FrontMaterial.specular * LightSource.specular *pow( NdotHV, FrontMaterial.shininess );
+      }
+   }
+
+   // assigner la couleur finale
+   FragColor = clamp( coul, 0.0, 1.0 );
+
+#if ( AFFICHENORMALES == 1 )
+   // pour le débogage
+   FragColor = vec4(N,1.0);
+#endif
+}
---
+++ tp5-tessellation/src/nuanceurTessEval.glsl
@@ -0,0 +1,186 @@
+//#version 410
+
+layout(quads) in;
+
+// Définition des paramètres des sources de lumière
+layout (std140) uniform LightSourceParameters
+{
+   vec4 ambient;
+   vec4 diffuse;
+   vec4 specular;
+   vec4 position[2];
+} LightSource;
+
+// Définition des paramètres globaux du modèle de lumière
+layout (std140) uniform LightModelParameters
+{
+   vec4 ambient;       // couleur ambiante
+   bool localViewer;   // observateur local ou à l'infini?
+   bool twoSide;       // éclairage sur les deux côtés ou un seul?
+} LightModel;
+
+// Declaration implicite de ces variables
+// in vec3 gl_TessCoord;
+// in int gl_PatchVerticesIn;
+// in int gl_PrimitiveID;
+// patch in float gl_TessLevelOuter[4];
+// patch in float gl_TessLevelInner[2];
+// in gl_PerVertex
+// {
+//   vec4 gl_Position;
+//   float gl_PointSize;
+//   float gl_ClipDistance[];
+// } gl_in[gl_MaxPatchVertices];
+
+// out gl_PerVertex {
+//   vec4 gl_Position;
+//   float gl_PointSize;
+//   float gl_ClipDistance[];
+// };
+
+uniform mat3 matrNormale;
+
+uniform float facteurDeform;
+uniform sampler2D textureDepl;
+
+out Attribs {
+   vec2 texCoord;
+   vec3 normale;
+} AttribsOut;
+
+float interpole( float v0, float v1, float v2, float v3 )
+{
+   float v01 = mix( v0, v1, gl_TessCoord.x );
+   float v32 = mix( v3, v2, gl_TessCoord.x );
+   return mix( v01, v32, gl_TessCoord.y );
+}
+vec2 interpole( vec2 v0, vec2 v1, vec2 v2, vec2 v3 )
+{
+   vec2 v01 = mix( v0, v1, gl_TessCoord.x );
+   vec2 v32 = mix( v3, v2, gl_TessCoord.x );
+   return mix( v01, v32, gl_TessCoord.y );
+}
+vec3 interpole( vec3 v0, vec3 v1, vec3 v2, vec3 v3 )
+{
+   vec3 v01 = mix( v0, v1, gl_TessCoord.x );
+   vec3 v32 = mix( v3, v2, gl_TessCoord.x );
+   return mix( v01, v32, gl_TessCoord.y );
+}
+vec4 interpole( vec4 v0, vec4 v1, vec4 v2, vec4 v3 )
+{
+   vec4 v01 = mix( v0, v1, gl_TessCoord.x );
+   vec4 v32 = mix( v3, v2, gl_TessCoord.x );
+   return mix( v01, v32, gl_TessCoord.y );
+}
+
+// affichage de la fonction paramétrique
+uniform vec4 bDim;
+vec3 FctParam( vec2 uv )
+{
+   const float PI = 3.141592654;
+   vec3 p = vec3(0);
+#if ( INDICEFONCTION == 1 )
+   // sphere
+   float s = uv.x * 2.*PI;
+   float t = uv.y * PI;
+   p = vec3( sin(t)*cos(s),
+             sin(t)*sin(s),
+             cos(t) * facteurDeform );
+#elif ( INDICEFONCTION == 2 )
+   // tore
+   float s = uv.x * 2.*PI;
+   float t = uv.y * 2.*PI;
+   p = vec3( ( 0.5*cos(s) + 1.0 )*cos(t),
+             ( 0.5*cos(s) + 1.0 )*sin(t),
+             0.5*sin(s) * facteurDeform );
+#elif ( INDICEFONCTION == 3 )
+   // pomme
+   float s = uv.x * 2.*PI;
+   float t = uv.y * 2.*PI - PI;
+   p = 0.1 * vec3( cos(s) * (4 + 3.8 * cos(t)) * facteurDeform,
+                   sin(s) * (4 + 3.8 * cos(t)) * facteurDeform,
+                   (cos(t)+sin(t)-1)*(1+sin(t))*log(1-PI*t/10)+7.5*sin(t) );
+#elif ( INDICEFONCTION == 4 )
+   // coquille
+   float s = -6. + ( 1.1*PI + 6. ) * uv.x;
+   float t = uv.y * PI;
+   p = pow(4./3.,s) * vec3( sin(t)*sin(t)*cos(s) * facteurDeform,
+                            sin(t)*sin(t)*sin(s) * facteurDeform,
+                            sin(t)*cos(t) );
+#elif ( INDICEFONCTION == 5 )
+   // serpent
+   float s = uv.x;
+   float t = uv.y * 2.*PI;
+   p = vec3( (1.-s) * (2.+cos(t)) * cos(2*PI*s)/2.,
+             (1.-s) * (2.+cos(t)) * sin(2*PI*s)/2.,
+             2.*s + (1.-s) * sin(t)/2. * facteurDeform );
+#elif ( INDICEFONCTION == 6 )
+   // trefoil
+   float s = uv.x * 2.*PI - PI;
+   float t = uv.y * 2.*PI - PI;
+   p = vec3( 0.75 * (sin(s) + 2*sin(2*s)) / (2+cos(t+2*PI/3)),
+             sin(3*s)/(2+cos(t)),
+             facteurDeform * (cos(s)-2*cos(2*s))*(2+cos(t))*(2+cos(t+2*PI/3))/8. );
+#endif
+   return p;
+}
+
+// déplacement du plan selon la texture
+float FctText( vec2 texCoord )
+{
+   //...
+   return 0.0; // à modifier!
+}
+
+void main( void )
+{
+   const float eps = 0.01;
+
+   // interpoler la position selon la tessellation (dans le repère de modélisation)
+   vec4 posModel = interpole( gl_in[0].gl_Position, gl_in[1].gl_Position, gl_in[2].gl_Position, gl_in[3].gl_Position );
+   vec2 uv = posModel.xy;
+
+   // générer (en utilisant directement posModel.xy) les coordonnées de texture plutôt que les interpoler
+   //AttribsOut.texCoord = ...;
+
+#if ( INDICEFONCTION != 0 )
+
+   // affichage de la fonction paramétrique (partie 1)
+   // ....xyz = FctParam( ... );
+   posModel.xyz = FctParam(uv);
+
+   // calculer la normale
+   vec2 epsX = vec2(eps, 0.0);
+   vec2 epsY = vec2(0.0, eps);
+   vec3 N = cross(FctParam(uv + epsY) - FctParam(uv - epsY),
+                  FctParam(uv + epsX) - FctParam(uv - epsX)); // à modifier
+
+#else
+
+   // déplacement selon la texture (partie 2)
+   // ....z = FctText( ... );
+
+//    posModel.x += bDim.x;
+//    posModel.y += bDim.y;
+    posModel.x -= .5;
+    posModel.y -= .5;
+
+    posModel.xy *= 2*bDim.xy;
+
+   // calculer la normale
+    vec2 epsX = vec2(eps, 0.0);
+    vec2 epsY = vec2(0.0, eps);
+//    vec3 N = cross(FctParam(uv + epsY) - FctParam(uv - epsY),
+//                  FctParam(uv + epsX) - FctParam(uv - epsX)); // à modifier
+    vec3 N = vec3(0,0,-1.0);
+
+#endif
+
+#if ( AFFICHENORMALES == 1 )
+   AttribsOut.normale = N;
+#else
+   AttribsOut.normale = matrNormale * N;
+#endif
+
+   gl_Position = posModel;
+}
